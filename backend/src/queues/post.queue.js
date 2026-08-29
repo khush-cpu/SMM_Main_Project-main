af0255031e123268a092eead280729b26634bc2c
itsocialontable <it@socialontable.com>
2026-08-29 17:16:45 +0530

    Organize backend code into backend_Web1 folder

--- backend/src/queues/post.queue.js
+++
@@ -1,6 +0,0 @@
-const { Queue } = require("bullmq");
-const connection = require("../config/redis.config");
-
-const postQueue = new Queue("post-queue", { connection });
-
-module.exports = postQueue;
